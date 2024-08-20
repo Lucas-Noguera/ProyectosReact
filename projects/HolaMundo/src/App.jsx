5e6e00f269183fa0357771a24c39de9dc7b8d282
Lucas-Noguera <lucas2005n@gmail.com>
2024-08-20 14:26:56 -0400

    Mi primer proyecto React terminado

--- projects/HolaMundo/src/App.jsx
+++ projects/HolaMundo/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
+const users = [
+{
+    userName: 'Lucas-Noguera',
+    name: 'Luba26',
+    isFollowing: true
+},
+{
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false,
+     
+},
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+{
+    userName: 'PacoHdez',
+    name: 'Paco Hdez',
+    isFollowing: true,
+},
+{
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false,
 }
 
-export default App
+
+]
+
+export function App () {
+    return(
+        <section className='App'>
+            {
+                users.map(user => {
+                    const {userName, name, isFollowing} = user
+                    return(
+                        <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialisFollowing={isFollowing}>
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+        </section>
+       
+)
+}
