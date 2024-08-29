705951be342952e2ee5c7214d428266ed9e55d43
Lucas-Noguera <lucas2005n@gmail.com>
2024-08-29 14:28:50 -0400

    a

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -1,48 +1,48 @@
-  import './App.css'
-  import {useState} from "react"
-  import confetti from 'canvas-confetti'
-  import { Square } from './Componentes/Square'
-  import { TURNS } from './constantes'
-  import {checkWinnerFrom, checkGameisOver} from './logic/board.js'
-  import { WinnerModal } from './Componentes/WinnerModal.jsx'
-  import { BoardComponent } from './Componentes/BoardComponent.jsx'
+import './App.css'
+import { useState } from "react"
+import confetti from 'canvas-confetti'
+import { Square } from './Componentes/Square'
+import { TURNS } from './constantes'
+import { checkWinnerFrom, checkGameisOver } from './logic/board.js'
+import { WinnerModal } from './Componentes/WinnerModal.jsx'
+import { BoardComponent } from './Componentes/BoardComponent.jsx'
 
 
-  function App() {
-    const [board, setBoard] =  useState(() => {
-      const boardFromStorage = window.localStorage.getItem('board')
-      return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
-    });
-    const [turn, setTurn] = useState(() => {
-      const turnFromLocalStorage = window.localStorage.getItem('turn')
-      return turnFromLocalStorage ?? TURNS.X
-    })
-    const [winner, setWinner] = useState(null)  
+function App() {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromLocalStorage = window.localStorage.getItem('turn')
+    return turnFromLocalStorage ?? TURNS.X
+  })
+  const [winner, setWinner] = useState(null)
 
-    const updateBoard = (index) => { 
-      if(board[index] || winner) return
-      const newBoard = [...board]
-      newBoard[index] = turn
-      setBoard(newBoard)
-      const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-      setTurn(newTurn)
-      window.localStorage.setItem('board', JSON.stringify(newBoard))
-      window.localStorage.setItem('turn', newTurn)
-      const newWinner = checkWinnerFrom(newBoard)
-      if(newWinner){
-        setWinner(newWinner);
-        confetti()
-      } else if(checkGameisOver(newBoard)){
-        setWinner(false);
-      }
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      setWinner(newWinner);
+      confetti()
+    } else if (checkGameisOver(newBoard)) {
+      setWinner(false);
     }
-    const resetGame = () => {
-      setBoard(Array(9).fill(null))
-      setTurn(TURNS.X)
-      setWinner(null)
-      window.localStorage.removeItem('board')
-      window.localStorage.removeItem('turn')
-    }
+  }
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+  }
 
   return (
     <main className="board">
@@ -53,9 +53,9 @@
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
       </section>
-          <WinnerModal resetGame={resetGame} winner={winner}/>
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   )
-  }
+}
 
-  export default App
+export default App
--- projects/tic-tac-toe/src/main.jsx
+++ projects/tic-tac-toe/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react"
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -6,5 +6,5 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
