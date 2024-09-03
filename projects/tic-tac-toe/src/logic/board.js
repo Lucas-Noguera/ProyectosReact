aaa9208d387d8a3f44c53466ec9086ffe034703c
Lucas-Noguera <lucas2005n@gmail.com>
2024-09-03 09:14:35 -0400

    Proyecto terminado

--- projects/tic-tac-toe/src/logic/board.js
+++ projects/tic-tac-toe/src/logic/board.js
@@ -1,19 +1,17 @@
-import { WINNER_COMBOS } from "../constantes";
-
+import { WINNER_COMBOS } from '../constantes'
 
 export const checkWinnerFrom = (boardToCheck) => {
-    for(const combo of WINNER_COMBOS){
-      const [a, b, c] = combo
-      if(boardToCheck[a] && 
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (boardToCheck[a] &&
         boardToCheck[a] === boardToCheck[b] &&
-      boardToCheck[a] === boardToCheck[c]){
-        return boardToCheck[a]
-      }
+      boardToCheck[a] === boardToCheck[c]) {
+      return boardToCheck[a]
     }
-    return  null
-
   }
+  return null
+}
 
- export const checkGameisOver = (newBoard) => {
-    return newBoard.every((square) => square !== null)
-   }
+export const checkGameisOver = (newBoard) => {
+  return newBoard.every((square) => square !== null)
+}
