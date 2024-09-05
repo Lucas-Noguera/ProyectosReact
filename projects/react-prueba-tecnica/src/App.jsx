5f0dee7d5916f3e229660deed64812ec1a116b34
Lucas-Noguera <lucas2005n@gmail.com>
2024-09-05 09:45:40 -0400

    Commit de actualizacion, problemas con el fetch de la URL

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+import { getRandomFact } from './services/facts'
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
+    getRandomFact().then(setFact)
   }, [])
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
 
   useEffect(() => {
     if (!fact) return
@@ -33,6 +32,7 @@ export function App () {
   return (
     <main>
       <h1>App de gatitos</h1>
+      <button onClick={handleClick}>Get new fact</button>
       {fact && <p>{fact}</p>}
       {imageUrl && <img src={imageUrl} alt={`imagen creada a partir  de las primeras tres palabras de "${fact}"`} />}
     </main>
