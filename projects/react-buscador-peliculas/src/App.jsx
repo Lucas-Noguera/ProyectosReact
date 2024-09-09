26926f313bf4fa4b0c80b11509ba609f158556cc
Lucas-Noguera <lucas2005n@gmail.com>
2024-09-09 11:41:57 -0400

    Prueba de buscador iniciado

---
+++ projects/react-buscador-peliculas/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className='page'>
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form action='' className='form'>
+          <input placeholder='Matrix, Avengers, Jhon Wick...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+
+      <main />
+    </div>
+  )
+}
+
+export default App
