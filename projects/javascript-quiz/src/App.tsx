58e0f8105e2100a2f622f2083fd817f4480df84a
Lucas-Noguera <lucas2005n@gmail.com>
2024-12-10 14:32:03 -0300

    Primer commit del nuevo proyecto, interfaz utilizando material UI

--- projects/javascript-quiz/src/App.tsx
+++ projects/javascript-quiz/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Container, Stack, Typography} from '@mui/material'
+import { JavaScriptLogo } from './JavasciptLogo'
+import { Start } from './Start'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <Container>
+        <Stack direction='row' alignItems='center' justifyContent='center' gap={2}>
+          <JavaScriptLogo />
+          <Typography variant='h6' component='h1' >
+            <h1>JavaScript Quiz</h1>
+          </Typography>
+        </Stack>
+        <Start />
+      </Container>
+    </main>
   )
-}
+} 
 
 export default App
