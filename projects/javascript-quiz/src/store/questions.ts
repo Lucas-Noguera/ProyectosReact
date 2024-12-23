6ffe5b294716e749a98cfc10823e62c640d72492
Lucas-Noguera <lucas2005n@gmail.com>
2024-12-23 14:29:14 -0300

    Commit

--- projects/javascript-quiz/src/store/questions.ts
+++ projects/javascript-quiz/src/store/questions.ts
@@ -22,7 +22,7 @@ export const useQuestionsStore = create<State>()(
       currentQuestion: 0,
 
       fetchQuestions: async (limit: number) => {
-        const res = await fetch('http://localhost:5173/data.json')
+        const res = await fetch('/data.json')
         const json = await res.json()
 
         const questions = json
